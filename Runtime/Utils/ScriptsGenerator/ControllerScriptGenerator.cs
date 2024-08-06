a9ed5d265ae75026bc001fc592330369a53304d5
Alexsey <zhitko.lesha322@gmail.com>
2024-08-06 18:47:33 +0400

    Separated scripts generator

--- Runtime/Utils/ScriptsGenerator/ControllerScriptGenerator.cs
+++ Runtime/Utils/ScriptsGenerator/ControllerScriptGenerator.cs
@@ -1,7 +1,7 @@
 using UnityEditor;
 using UnityEngine;
 
-public static class ScriptsGenerator
+public static class ControllerScriptGenerator
 {
     [MenuItem("Assets/Create/CustomController C# Script", false, 80)]
     public static void CreateCustomScript()
@@ -27,54 +27,6 @@ public static class ScriptsGenerator
 using Composite.Core;
 
 public class {className} : AbstractController
-{{
-    
-}}
-";
-    }
-
-    private static string GetSelectedPathOrFallback()
-    {
-        string path = "Assets";
-        foreach (Object obj in Selection.GetFiltered(typeof(Object), SelectionMode.Assets))
-        {
-            path = AssetDatabase.GetAssetPath(obj);
-            if (!string.IsNullOrEmpty(path) && System.IO.File.Exists(path))
-            {
-                path = System.IO.Path.GetDirectoryName(path);
-                break;
-            }
-        }
-        return path;
-    }
-    
-    
-    
-    
-    [MenuItem("Assets/Create/CustomView C# Script", false, 80)]
-    public static void CreateCustomScript()
-    {
-        string path = GetSelectedPathOrFallback();
-        
-        string scriptName = "NewCustomView.cs";
-        string filePath = AssetDatabase.GenerateUniqueAssetPath($"{path}/{scriptName}");
-        
-        string template = GetScriptTemplate("NewCustomView");
-        
-        System.IO.File.WriteAllText(filePath, template);
-        AssetDatabase.Refresh();
-        
-        Object asset = AssetDatabase.LoadAssetAtPath<Object>(filePath);
-        Selection.activeObject = asset;
-    }
-
-    private static string GetScriptTemplate(string className)
-    {
-        return 
-            $@"using UnityEngine;
-using Composite.Core;
-
-public class {className} : AbstractView
 {{
     
 }}
